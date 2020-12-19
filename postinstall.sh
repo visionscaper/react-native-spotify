64c17f374cd261f40f40da6a7f64c2b91dad7dd9
Sjaak Meulen <jacobus@kadima-tech.com>
2020-12-19 23:34:05 +0100

    Fixed broken error condition on git pull

--- postinstall.sh
+++ postinstall.sh
@@ -5,15 +5,16 @@ echo "$MODULES" | while IFS= read -r line; do
   if [[ $line =~ (.*)\$\$\$(.*) ]] ; then
     path=$(echo ${BASH_REMATCH[1]} | sed 's/.*= //')
     url=$(echo ${BASH_REMATCH[2]} | sed 's/.*= //')
-    if [ -d $path ] ; then
+    echo $PWD
+    echo $CWD
+    if [ -f "$path/.git" ] ; then
       echo "Updating submodule"
       start=$PWD;
-      cd $path;
-      git pull;
-      cd $PWD;
+      (cd $path && git pull && cd $PWD)
     else
-      echo "Cloning submodule $url to $path"
-      git clone $url $path;
+    echo $path
+      echo "Cloning submodule $url to $path" "$path/.git"
+      git clone $url $path
     fi
   fi
 done
